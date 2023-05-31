63e78deb0392130f33e0adece249a3d448cda9ff
LongNT2003 <long02042003@gmail.com>
2023-06-01 00:48:38 +0700

    after change

---
+++ OtherProjects/Lab1/StarTree.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class StarTree{
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("input n: ");
+        int n = keyboard.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int m = 0; m <n-i; m++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2*i-1; k++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+
+}
