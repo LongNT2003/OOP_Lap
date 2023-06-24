2a0597dd1ab05dd76b610adfc51d0a4e6e765aec
LongNT2003 <130909210+LongNT2003@users.noreply.github.com>
2023-06-24 14:38:10 +0700

    d

--- AimsProject/src/hust/soict/dsai/aims/screen/MediaStore.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/MediaStore.java
@@ -3,18 +3,22 @@ package aims.screen;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import aims.cart.Cart;
 import aims.media.Media;
 import aims.media.Playable;
 
-public class MediaStore extends JPanel{
+public class MediaStore extends JPanel implements ActionListener{
     private Media media;
     public MediaStore(Media media){
         this.media=media;
@@ -30,9 +34,15 @@ public class MediaStore extends JPanel{
         JPanel container = new JPanel();
         container.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        container.add(new JButton("Add to cart"));
+        JButton addToCartButton = new JButton("Add to cart");
+        addToCartButton.addActionListener(this); // Registering the ActionListener
+        container.add(addToCartButton);
         if (media instanceof Playable){
-            container.add(new JButton("Play"));
+
+            JButton playButton = new JButton("Play");
+            playButton.addActionListener(this); // Registering the ActionListener
+            container.add(playButton);
+
         }
 
         this.add(Box.createVerticalGlue());
@@ -42,6 +52,29 @@ public class MediaStore extends JPanel{
         this.add(container);
         
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Add to cart")) {
+            // Handle the "Add to cart" button action here
+            JDialog dialog = new JDialog();
+            dialog.setTitle("Added to Cart");
+            JLabel cartLabel = new JLabel("Item added to cart: " + media.getTitle());
+            dialog.getContentPane().add(cartLabel);
+            dialog.setSize(300, 200);
+            dialog.setLocationRelativeTo(null); // Center the dialog on the screen
+            dialog.setVisible(true);
+        } else if (e.getActionCommand().equals("Play")) {
+            // Handle the "Play" button action here
+            JDialog dialog = new JDialog();
+            JLabel l = new JLabel("Playing: " + media.getTitle());
+            l.setHorizontalAlignment(JLabel.CENTER);
+            l.setVerticalAlignment(JLabel.CENTER);
+            dialog.getContentPane().add(l);
+            dialog.setSize(300, 200);
+            dialog.setLocationRelativeTo(null); // Center the dialog on the screen
+            dialog.setVisible(true);
+        }
+    }  
 }
