6cb63ac65696adac98e08caf57b86892dab02e0f
LongNT2003 <130909210+LongNT2003@users.noreply.github.com>
2023-06-25 12:40:55 +0700

    Update Playable.java

--- AimsProject/src/hust/soict/dsai/aims/media/Playable.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Playable.java
@@ -1,5 +1,6 @@
-package aims.media;
+package src.hust.soict.dsai.aims.media;
 
+import src.hust.soict.dsai.aims.exception.PlayerException;
 public interface Playable {
-    public void play();
+    public void play() throws PlayerException;
 }
